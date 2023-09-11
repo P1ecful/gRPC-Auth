1e24fa6f6190b5f0f637c27ad29df3184b7709c7
Dmitrii <101904396+P1ecful@users.noreply.github.com>
2023-09-11 16:36:40 +0300

    Update users.go
    Test version of users.go file. 
    Changes in logic

--- internal/db/users.go
+++ internal/db/users.go
@@ -7,18 +7,48 @@ import (
 	_ "modernc.org/sqlite"
 )
 
-func AddMeta(login string, password string) {
-	db, err := sql.Open("sqlite", "usersmeta.db")
-	if err != nil {
+type UserMeta struct {
+	login string
+	password string
+}
+
+func ConnQuery(){
+	db, err := sql.Open("sqlite", "usermeta.db")
+	if err !- nil {
 		log.Fatal(err)
 	}
 
 	defer db.Close()
+}
 
-	_, err = db.Exec("insert into Meta (login, password) values ($1, $2)", login, password)
+
+func (u *UserMeta) AddMeta() {
+	// db, err := sql.Open("sqlite", "usersmeta.db")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// defer db.Close()
+	ConnQuery()
+	_, err = db.Exec("insert into Meta (login, password) values ($1, $2)", u.login, u.password)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	log.Println("Succesful register")
 }
+
+func (u *UserMeta) CheckMeta {
+	// db, err := sql.Open("sqlite", "usersmeta.db")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// defer db.Close()
+	ConnQuery()
+	_, err = db.QueryRow("select * form Meta where login = $1", u.login)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
